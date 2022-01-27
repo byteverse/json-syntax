b6ca33f93695e8541f5ac6fe5215e45a4c930ae9
Eric Demko <edemko@layer3com.com>
2022-01-27 16:11:09 -0500

    possibly-okay encoding of bigdecimals from small scientific

--- src/Json/Smile.hs
+++ src/Json/Smile.hs
@@ -7,16 +7,20 @@ module Json.Smile
 
 import Prelude hiding (Bool(..))
 
+import Data.Bits (finiteBitSize,unsafeShiftL)
 import Data.Bytes.Builder (Builder)
 import Data.Foldable (foldMap')
+import Data.Functor ((<&>))
+import Data.Int (Int32)
 import Data.Text.Short (ShortText)
 import Data.Word (Word8)
 import Json (Value(..), Member(..))
 
+import qualified Data.Bytes as Bytes
 import qualified Data.Bytes.Builder as B
 import qualified Data.Bytes.Text.Ascii as Ascii
-import qualified Data.Number.Scientific as Sci
 import qualified Data.ByteString.Short as SBS
+import qualified Data.Number.Scientific as Sci
 import qualified Data.Text.Short as TS
 
 -- | Encode a Json 'Value' to the Smile binary format.
@@ -24,7 +28,7 @@ import qualified Data.Text.Short as TS
 encodeSimple :: Value -> Builder
 encodeSimple v0 = header <> recurse v0
   where
-  header = B.bytes $ Ascii.fromString ":)\n\x04"
+  header = B.bytes $ Ascii.fromString ":)\n\x00"
   recurse :: Value -> Builder
   recurse (Object obj) = B.word8 0xFA <> foldMap' recMember obj <> B.word8 0xFB
   recurse (Array arr) = B.word8 0xF8 <> foldMap' recurse arr <> B.word8 0xF9
@@ -34,13 +38,27 @@ encodeSimple v0 = header <> recurse v0
       = B.word8 0x24 <> B.int32LEB128 i32
     | Just i64 <- Sci.toInt64 x
       = B.word8 0x25 <> B.int64LEB128 i64
-    | otherwise = errorWithoutStackTrace
-        ("TODO Smile encoding only supports Int32/Int64 numbers for now: " ++ show x)
+    | otherwise = Sci.withExposed encodeSmall encodeBig x
   recurse Null = B.word8 0x21
   recurse False = B.word8 0x22
   recurse True = B.word8 0x23
   recMember :: Member -> Builder
   recMember Member{key,value} = encodeKeySimple key <> recurse value
+  encodeSmall :: Int -> Int -> Builder
+  encodeSmall c e
+    =  B.word8 0x2A -- token tag
+    <> B.int32LEB128 (fromIntegral @Int @Int32 e) -- zigzag scale, WARNING performs modulo since SMILE doesn't support exponents larger than 32-bit
+    <> B.wordLEB128 (fromIntegral @Int @Word nCoeffBytes)
+    <> B.sevenEightSmile (Bytes.fromShortByteString $ SBS.pack coeffBytes)
+    where
+    nCoeffBytes = finiteBitSize c `div` 8
+    -- TODO probably bounded builder
+    coeffBytes :: [Word8] -- big-endian
+    coeffBytes = [nCoeffBytes - 1 .. 0] <&> \i ->
+      fromIntegral @Int @Word8 (unsafeShiftL c (8*i))
+  encodeBig :: Integer -> Integer -> Builder
+  encodeBig c e = errorWithoutStackTrace ("TODO Smile encoding only supports numbers that \
+                \can be represented by Int*10^Int for now: " ++ show (Sci.large c e))
 
 encodeKeySimple :: ShortText -> Builder
 encodeKeySimple str = case SBS.length (TS.toShortByteString str) of
