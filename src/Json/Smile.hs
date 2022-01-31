4154e4092678da4841890b9da9247a951d03411e
Eric Demko <edemko@layer3com.com>
2022-01-31 17:40:08 -0500

    BigDecimal encoding, but tests fail to encode scale as expected

--- src/Json/Smile.hs
+++ src/Json/Smile.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeApplications #-}
 
@@ -7,16 +9,19 @@ module Json.Smile
 
 import Prelude hiding (Bool(..))
 
-import Data.Bits (finiteBitSize,unsafeShiftL,unsafeShiftR,(.&.),(.|.))
+import Data.Bits (unsafeShiftR,(.&.),(.|.))
 import Data.Bytes.Builder (Builder)
 import Data.Foldable (foldMap')
-import Data.Functor ((<&>))
 import Data.Int (Int32)
+import Data.Primitive.ByteArray (ByteArray,newByteArray,byteArrayFromListN,sizeofByteArray)
+import Data.Primitive.ByteArray (MutableByteArray(..),unsafeFreezeByteArray)
 import Data.Text.Short (ShortText)
 import Data.Word (Word8,Word32)
-import Data.Word.Zigzag (toZigzag,toZigzag32,toZigzag64)
+import Data.Word.Zigzag (toZigzag32,toZigzag64)
+import GHC.Word (Word(..))
 import Json (Value(..), Member(..))
 import Numeric.Natural (Natural)
+import System.IO.Unsafe (unsafeDupablePerformIO)
 
 import qualified Data.Bytes as Bytes
 import qualified Data.Bytes.Builder as B
@@ -24,6 +29,7 @@ import qualified Data.Bytes.Text.Ascii as Ascii
 import qualified Data.ByteString.Short as SBS
 import qualified Data.Number.Scientific as Sci
 import qualified Data.Text.Short as TS
+import qualified GHC.Integer.GMP.Internals as GMP
 
 -- | Encode a Json 'Value' to the Smile binary format.
 -- This encoder does not produce backreferences.
@@ -34,42 +40,66 @@ encodeSimple v0 = header <> recurse v0
   recurse :: Value -> Builder
   recurse (Object obj) = B.word8 0xFA <> foldMap' recMember obj <> B.word8 0xFB
   recurse (Array arr) = B.word8 0xF8 <> foldMap' recurse arr <> B.word8 0xF9
-  recurse (String str) = B.word8 0xE4 <> B.shortTextUtf8 str <> B.word8 0xFC
+  recurse (String str) = encodeStringSimple str
   recurse (Number x)
+    | Just i32 <- Sci.toInt32 x
+    , -16 <= i32 && i32 <= 15
+    , w5 <- fromIntegral @Word32 @Word8 (toZigzag32 i32)
+      = B.word8 (0xC0 + w5)
     | Just i32 <- Sci.toInt32 x
       = B.word8 0x24 <> vlqSmile (fromIntegral $ toZigzag32 i32)
     | Just i64 <- Sci.toInt64 x
       = B.word8 0x25 <> vlqSmile (fromIntegral $ toZigzag64 i64)
-    | otherwise = Sci.withExposed encodeSmall encodeBig x
+    | otherwise = Sci.withExposed encodeSmallDecimal encodeBigDecimal x
   recurse Null = B.word8 0x21
   recurse False = B.word8 0x22
   recurse True = B.word8 0x23
   recMember :: Member -> Builder
   recMember Member{key,value} = encodeKeySimple key <> recurse value
-  encodeSmall :: Int -> Int -> Builder
-  encodeSmall c e
+  encodeSmallDecimal :: Int -> Int -> Builder
+  encodeSmallDecimal c e = encodeBigDecimal (fromIntegral c) (fromIntegral e)
+  encodeBigDecimal :: Integer -> Integer -> Builder
+  encodeBigDecimal c e
     =  B.word8 0x2A -- bigdecimal token tag
-    <> vlqSmile (fromIntegral @Word32 @Natural $ toZigzag32 scale) -- exponent
-    <> vlqSmile (fromIntegral @Int @Natural nCoeffBytes) -- size of byte array
-    <> B.sevenEightSmile (Bytes.fromShortByteString $ SBS.pack coeffBytes)
+    <> vlqSmile ( fromIntegral @Word32 @Natural
+                $ toZigzag32 scale)
+    <> vlqSmile (fromIntegral @Int @Natural $ sizeofByteArray coeffBytes) -- size of byte digits
+    <> B.sevenEightSmile (Bytes.fromByteArray coeffBytes) -- 7/8 encoding of byte digits
     where
-    -- | WARNING performs modulo since SMILE doesn't support exponents larger than 32-bit
-    scale = fromIntegral @Int @Int32 e
-    nCoeffBytes =
-      let nbits = finiteBitSize c
-          nbytes = nbits `div` 8
-       in if nbits `mod` 8 == 0 then nbytes else nbytes + 1
-    coeffBytes :: [Word8] -- big-endian
-    coeffBytes = [nCoeffBytes - 1 .. 0] <&> \i ->
-      fromIntegral @Int @Word8 (unsafeShiftL c (8*i))
-  encodeBig :: Integer -> Integer -> Builder
-  encodeBig c e = errorWithoutStackTrace ("TODO Smile encoding only supports numbers that \
-                \can be represented by Int*10^Int for now: " ++ show (Sci.large c e))
+    scale :: Int32
+    -- WARNING smile can't handle exponents outside int32_t, so this truncates
+    -- WARNING "scale" is what Java BigDecimal thinks, which is negative of all mathematics since exponential notation was invented 💩
+    scale = fromIntegral @Integer @Int32 (-e)
+    coeffBytes :: ByteArray
+    coeffBytes =
+      if c == 0 then byteArrayFromListN 1 [0::Word8]
+      else unsafeDupablePerformIO $ do
+        let nDigits256 = W# (GMP.sizeInBaseInteger c 256#)
+        mut <- newByteArray (fromIntegral @Word @Int nDigits256)
+        let !(MutableByteArray mut#) = mut
+        !_ <- GMP.exportIntegerToMutableByteArray c mut# 0## 1#
+        unsafeFreezeByteArray mut
+
+encodeStringSimple :: ShortText -> Builder
+encodeStringSimple str = case SBS.length (TS.toShortByteString str) of
+  0 -> B.word8 0x20
+  n | n <= 64
+    && TS.isAscii str
+    , w8 <- fromIntegral @Int @Word8 (n - 1)
+    -> B.word8 (0x40 + w8) <> B.shortTextUtf8 str
+    | n <= 65
+    , w8 <- fromIntegral @Int @Word8 (n - 2)
+    -> B.word8 (0x80 + w8) <> B.shortTextUtf8 str
+    | otherwise
+    -> B.word8 0xE4 <> B.shortTextUtf8 str <> B.word8 0xFC
 
 encodeKeySimple :: ShortText -> Builder
 encodeKeySimple str = case SBS.length (TS.toShortByteString str) of
   0 -> B.word8 0x20
-  1 -> B.word8 0x80 <> B.shortTextUtf8 str
+  n | n <= 64
+    && TS.isAscii str
+    , w8 <- fromIntegral @Int @Word8 (n - 1)
+    -> B.word8 (0x80 + w8) <> B.shortTextUtf8 str
   n | n < 56
     , w8 <- fromIntegral @Int @Word8 (n - 2)
     -> B.word8 (0xC0 + w8) <> B.shortTextUtf8 str
--- test/Main.hs
+++ test/Main.hs
@@ -3,13 +3,15 @@
 {-# language ScopedTypeVariables #-}
 
 import Control.Monad (when)
-import Data.ByteString.Short.Internal (ShortByteString(SBS))
 import Data.Bytes (Bytes)
+import Data.ByteString.Short.Internal (ShortByteString(SBS))
 import Data.Primitive (ByteArray(ByteArray))
 import Data.Scientific (Scientific,scientific)
-import Test.QuickCheck ((===))
 import Data.Text.Short (ShortText)
+import System.IO (withFile,IOMode(..))
+import Test.QuickCheck ((===))
 import Test.Tasty (defaultMain,testGroup,TestTree)
+import Test.Tasty.Golden (goldenVsFile)
 import Test.Tasty.HUnit ((@=?))
 import Twitter100 (encodedTwitter100,byteStringTwitter100)
 
@@ -17,11 +19,15 @@ import qualified Data.Aeson as AE
 import qualified Data.Bytes as Bytes
 import qualified Data.Bytes.Builder as Builder
 import qualified Data.Bytes.Chunks as BChunks
+import qualified Data.Bytes.Text.Ascii as Ascii
+import qualified Data.Bytes.Text.Latin1 as Latin1
+import qualified Data.ByteString as BS
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Number.Scientific as SCI
 import qualified Data.Text.Short as TS
 import qualified GHC.Exts as Exts
 import qualified Json as J
+import qualified Json.Smile as Smile
 import qualified Test.QuickCheck as QC
 import qualified Test.Tasty.HUnit as THU
 import qualified Test.Tasty.QuickCheck as TQC
@@ -31,76 +37,103 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests"
-  [ THU.testCase "A" $
-      Right (J.Object mempty)
-      @=?
-      J.decode (Bytes.fromAsciiString "{}")
-  , THU.testCase "B" $
-      Right (J.Object (Exts.fromList [J.Member "foo" J.True]))
-      @=?
-      J.decode (Bytes.fromAsciiString "{\"foo\" : true}")
-  , THU.testCase "C" $
-      Right (J.Array (Exts.fromList [J.String "bar"]))
-      @=?
-      J.decode (Bytes.fromAsciiString "[\"bar\"]")
-  , THU.testCase "D" $
-      Right (J.Object (Exts.fromList [J.Member "foo" J.True, J.Member "bar" J.False]))
-      @=?
-      J.decode (Bytes.fromAsciiString "{\"foo\" : true, \"bar\": false }")
-  , THU.testCase "E" $
-      Right (J.String "Smile: 😂")
-      @=?
-      J.decode (shortTextToBytes "\"Smile: 😂\"")
-  , THU.testCase "F" $
-      Right (J.Array (Exts.fromList [ J.Object mempty, J.Object mempty, J.Null ]))
-      @=?
-      J.decode (shortTextToBytes " [ {} , { } , null ] ")
-  , THU.testCase "G" $ case J.decode (shortTextToBytes " [ 55e2 , 1 ] ") of
-      Right (J.Array xs) -> case Exts.toList xs of
-        [J.Number a, J.Number b] -> do
-          SCI.toWord32 a @=? Just 5500
-          SCI.toWord32 b @=? Just 1
-        _ -> fail "no good y"
-      _ -> fail "no good x"
-  , THU.testCase "H" $ case J.decode (shortTextToBytes " [] x") of
-      Left _ -> pure ()
-      Right _ -> fail "this was not supposed parse"
-  , THU.testCase "I" $
-      BChunks.concat (Builder.run 1 (J.encode (J.Array mempty)))
-      @=?
-      Bytes.fromLatinString "[]"
-  , THU.testCase "J" $
-      BChunks.concat (Builder.run 1 (J.encode (J.Array mempty)))
-      @=?
-      Bytes.fromLatinString "[]"
-  , THU.testCase "K" $
-      BChunks.concat (Builder.run 1 (J.encode (J.String "Hello\DELWorld")))
-      @=?
-      Bytes.fromLatinString "\"Hello\DELWorld\""
-  , THU.testCase "L" $
-      BChunks.concat (Builder.run 1 (J.encode (J.String "Hello\nWorld")))
-      @=?
-      Bytes.fromLatinString "\"Hello\\nWorld\""
-  , TQC.testProperty "M" $ QC.forAll (jsonFromPrintableStrings <$> QC.vectorOf 10 QC.arbitrary) $ \val0 -> do
-      let enc = BChunks.concat (Builder.run 128 (J.encode val0))
-      case J.decode enc of
-        Left _ -> QC.property False
-        Right val1 -> val0 === val1
-  , TQC.testProperty "N" $ QC.forAll (jsonFromPrintableStrings <$> QC.vectorOf 400 QC.arbitrary) $ \val0 -> do
-      let enc = BChunks.concat (Builder.run 128 (J.encode val0))
-      case J.decode enc of
-        Left e -> QC.counterexample (show e) False
-        Right val1 -> val0 === val1
-  , TQC.testProperty "O" $ QC.forAll (jsonFromAsciiStrings <$> QC.vectorOf 10 QC.arbitrary) $ \val0 -> do
-      let enc = BChunks.concat (Builder.run 128 (J.encode val0))
-      case J.decode enc of
-        Left _ -> QC.property False
-        Right val1 -> val0 === val1
-  , TQC.testProperty "P" $ QC.forAll (jsonFromAsciiStrings <$> QC.vectorOf 400 QC.arbitrary) $ \val0 -> do
-      let enc = BChunks.concat (Builder.run 128 (J.encode val0))
-      case J.decode enc of
-        Left e -> QC.counterexample (show e) False
-        Right val1 -> val0 === val1
+  [ testGroup "poorly-named tests"
+    [ THU.testCase "A" $
+        Right (J.Object mempty)
+        @=?
+        J.decode (Ascii.fromString "{}")
+    , THU.testCase "B" $
+        Right (J.Object (Exts.fromList [J.Member "foo" J.True]))
+        @=?
+        J.decode (Ascii.fromString "{\"foo\" : true}")
+    , THU.testCase "C" $
+        Right (J.Array (Exts.fromList [J.String "bar"]))
+        @=?
+        J.decode (Ascii.fromString "[\"bar\"]")
+    , THU.testCase "D" $
+        Right (J.Object (Exts.fromList [J.Member "foo" J.True, J.Member "bar" J.False]))
+        @=?
+        J.decode (Ascii.fromString "{\"foo\" : true, \"bar\": false }")
+    , THU.testCase "E" $
+        Right (J.String "Smile: 😂")
+        @=?
+        J.decode (shortTextToBytes "\"Smile: 😂\"")
+    , THU.testCase "F" $
+        Right (J.Array (Exts.fromList [ J.Object mempty, J.Object mempty, J.Null ]))
+        @=?
+        J.decode (shortTextToBytes " [ {} , { } , null ] ")
+    , THU.testCase "G" $ case J.decode (shortTextToBytes " [ 55e2 , 1 ] ") of
+        Right (J.Array xs) -> case Exts.toList xs of
+          [J.Number a, J.Number b] -> do
+            SCI.toWord32 a @=? Just 5500
+            SCI.toWord32 b @=? Just 1
+          _ -> fail "no good y"
+        _ -> fail "no good x"
+    , THU.testCase "H" $ case J.decode (shortTextToBytes " [] x") of
+        Left _ -> pure ()
+        Right _ -> fail "this was not supposed parse"
+    , THU.testCase "I" $
+        BChunks.concat (Builder.run 1 (J.encode (J.Array mempty)))
+        @=?
+        Latin1.fromString "[]"
+    , THU.testCase "J" $
+        BChunks.concat (Builder.run 1 (J.encode (J.Array mempty)))
+        @=?
+        Latin1.fromString "[]"
+    , THU.testCase "K" $
+        BChunks.concat (Builder.run 1 (J.encode (J.String "Hello\DELWorld")))
+        @=?
+        Latin1.fromString "\"Hello\DELWorld\""
+    , THU.testCase "L" $
+        BChunks.concat (Builder.run 1 (J.encode (J.String "Hello\nWorld")))
+        @=?
+        Latin1.fromString "\"Hello\\nWorld\""
+    , TQC.testProperty "M" $ QC.forAll (jsonFromPrintableStrings <$> QC.vectorOf 10 QC.arbitrary) $ \val0 -> do
+        let enc = BChunks.concat (Builder.run 128 (J.encode val0))
+        case J.decode enc of
+          Left _ -> QC.property False
+          Right val1 -> val0 === val1
+    , TQC.testProperty "N" $ QC.forAll (jsonFromPrintableStrings <$> QC.vectorOf 400 QC.arbitrary) $ \val0 -> do
+        let enc = BChunks.concat (Builder.run 128 (J.encode val0))
+        case J.decode enc of
+          Left e -> QC.counterexample (show e) False
+          Right val1 -> val0 === val1
+    , TQC.testProperty "O" $ QC.forAll (jsonFromAsciiStrings <$> QC.vectorOf 10 QC.arbitrary) $ \val0 -> do
+        let enc = BChunks.concat (Builder.run 128 (J.encode val0))
+        case J.decode enc of
+          Left _ -> QC.property False
+          Right val1 -> val0 === val1
+    , TQC.testProperty "P" $ QC.forAll (jsonFromAsciiStrings <$> QC.vectorOf 400 QC.arbitrary) $ \val0 -> do
+        let enc = BChunks.concat (Builder.run 128 (J.encode val0))
+        case J.decode enc of
+          Left e -> QC.counterexample (show e) False
+          Right val1 -> val0 === val1
+    ]
+  , testGroup "smile-encode" $
+    let mkTest name n = goldenVsFile name ("test/golden/"++n++"/expect.bin")
+                                          ("test/golden/"++n++"/out.bin")
+                                          (generate n)
+        generate n = do
+          contents <- Bytes.fromByteString <$> BS.readFile ("test/golden/"++n++"/in.json")
+          let json = case J.decode contents of
+                Left err -> error $ show err
+                Right it -> it
+              out = Smile.encodeSimple json
+          withFile ("test/golden/"++n++"/out.bin") WriteMode $ \fp ->
+            BChunks.hPut fp (Builder.run 2048 out)
+     in
+      [ mkTest "hello-world" "001"
+      , mkTest "konnichiwa-minnasan" "002"
+      , mkTest "konnichiwa-minnasan" "003"
+      , mkTest "\"sixty\"-char unicode string" "003"
+      , mkTest "\"sixty\"-char unicode keyname" "004"
+      , mkTest "small numbers" "005"
+      , mkTest "long positive decimal" "006"
+      , mkTest "long negative decimal" "007"
+      , mkTest "long positive integer" "008"
+      , mkTest "long negative integer" "009"
+      , mkTest "scientific-notation Int-repr" "010"
+      ]
   , THU.testCase "Twitter100" $
       case J.decode (Bytes.fromByteArray encodedTwitter100) of
         Left _ -> fail "nope"
